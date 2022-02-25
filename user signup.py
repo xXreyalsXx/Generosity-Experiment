6d1ba0a51e4544a6beac6d7e545392d9ed02189b
xXreyalsXx <95142788+xXreyalsXx@users.noreply.github.com>
2022-02-24 16:24:15 -0800

    Update 0.1
    Added a download feature so that the UserInfo dictionary will automatically be downloaded each time it is changed so that it can be accessed in the login program.

--- user signup.py
+++ user signup.py
@@ -2,6 +2,12 @@
 import random
 
 # defining user number
+# imports
+import pickle
+import random
+
+# user signup code
+# defining user number
 user_number = 0
 
 # creating UserInfo dictionary
@@ -15,32 +21,39 @@ user_IDs = [0, ]
 user_coins = 0
 
 # while loop so the dictionaries and lists don't get reset
-while 1 == 1:
-    # getting username
-    username = input("what would you like your username to be?: ")
+# getting username
+username = input("what would you like your username to be?: ")
 
-    # password and password length
+# password and password length
+password = input("what would you like your password to be?: ")
+while len(password) < 5:
+    print("your password is too short")
     password = input("what would you like your password to be?: ")
-    while len(password) < 5:
-        print("your password is too short")
-        password = input("what would you like your password to be?: ")
 
-    # rolling user ID
+# rolling user ID
+user_ID = random.randint(1, 10000000000)
+
+# checking if user_ID is in user_IDs
+while user_ID in user_IDs:
     user_ID = random.randint(1, 10000000000)
 
-    # checking if user_ID is in user_IDs
-    while user_ID in user_IDs:
-        user_ID = random.randint(1, 10000000000)
+# appending list with new ID
+user_IDs.append(user_ID)
 
-    # appending list with new ID
-    user_IDs.append(user_ID)
+# hashing information
+hashed_password = hash(password)
+hashed_user_ID = hash(user_ID)
+hashed_username = hash(username)
 
-    # hashing information
-    hashed_password = hash(password)
-    hashed_user_ID = hash(user_ID)
-    hashed_username = hash(username)
+# user info nested dictionaries
+user_number += 1
+UserInfo[user_number] = {'username': username, 'password': hashed_password, 'user_ID': hashed_user_ID, 'user_number': user_number}
 
-    # user info nested dictionaries
-    user_number += 1
-    UserInfo[user_number] = {'username': username, 'password': hashed_password, 'user_ID': hashed_user_ID, 'user_number': user_number}
-    print(UserInfo)
+# downloading UserInfo
+with open('saved_dictionary.pkl', 'wb') as f:
+    pickle.dump(UserInfo, f)
+
+# user login code
+# the username and password that the user is trying to  log in with
+login_username = input("what is your username?")
+login_password = input("what is your password")
