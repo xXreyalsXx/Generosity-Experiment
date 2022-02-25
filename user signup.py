babe570e2367d7e6a65704cf3bcf399f86fedc79
xXreyalsXx <95142788+xXreyalsXx@users.noreply.github.com>
2022-02-24 21:14:53 -0800

    Update 0.3
    Fixed an issue where unstable hashed would be used to hash the files. Also added while loop so that you can create as many accounts as you want.

--- user signup.py
+++ user signup.py
@@ -1,6 +1,7 @@
 # imports
 import pickle
 import random
+import hashlib
 
 # defining user number
 user_number = 0
@@ -15,34 +16,40 @@ user_IDs = [0, ]
 # user_coins variable
 user_coins = 0
 
-# getting username
-username = input("what would you like your username to be?: ")
 
-# password and password length
-password = input("what would you like your password to be?: ")
-while len(password) < 5:
-    print("your password is too short")
+while 1 == 1:
+
+    # getting username
+    username = input("what would you like your username to be?: ")
+
+    # password and password length
     password = input("what would you like your password to be?: ")
+    while len(password) < 5:
+        print("your password is too short")
+        password = input("what would you like your password to be?: ")
 
-# rolling user ID
-user_ID = random.randint(1, 10000000000)
-
-# checking if user_ID is in user_IDs
-while user_ID in user_IDs:
+    # rolling user ID
     user_ID = random.randint(1, 10000000000)
 
-# appending list with new ID
-user_IDs.append(user_ID)
+    # checking if user_ID is in user_IDs
+    while user_ID in user_IDs:
+        user_ID = random.randint(1, 10000000000)
 
-# hashing information
-hashed_password = hash(password)
-hashed_user_ID = hash(user_ID)
-hashed_username = hash(username)
+    # appending list with new ID
+    user_IDs.append(user_ID)
 
-# user info nested dictionaries
-user_number += 1
-UserInfo[user_number] = {'username': hashed_username, 'password': hashed_password, 'user_ID': hashed_user_ID, 'user_number': user_number}
+    # converting information from strings to bytes
+    encoded_username = username.encode(encoding = 'UTF-8', errors = 'strict')
+    encoded_password = password.encode(encoding = 'UTF-8', errors = 'strict')
 
-# downloading UserInfo
-with open('saved_dictionary.pkl', 'wb') as f:
-    pickle.dump(UserInfo, f)
+    # hashing information
+    hashed_username = hashlib.blake2b(encoded_username).hexdigest()
+    hashed_password = hashlib.blake2b(encoded_password).hexdigest()
+
+    # user info nested dictionaries
+    user_number += 1
+    UserInfo[user_number] = {'username': hashed_username, 'password': hashed_password, 'user_ID': user_ID, 'user_number': user_number, 'user_coins': user_coins}
+
+    # downloading UserInfo
+    with open('UserInfo.pkl', 'wb') as f:
+        pickle.dump(UserInfo, f)
